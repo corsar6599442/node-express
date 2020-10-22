47b4c0ef8f9f588f213e51e6224a6af843385e7a
corsar6599442 <corsar6599442@gmail.com>
2020-10-22 20:49:49 +0300

    промежуточный

--- routes/courses.js
+++ routes/courses.js
@@ -11,6 +11,19 @@ router.get('/', async (req, res) => {
 	})
 })
 
+router.get('/:id/edit', async (req, res) => {
+	if (!req.query.allow) {
+		return res.redirect('/')
+	}
+
+	const course = await Course.getById(req.params.id)
+
+	res.render('course-edit', {
+		title: `Редактировать ${course.title}`,
+		course
+	})
+})
+
 router.get('/:id', async (req, res) => {
 	const course = await Course.getById(req.params.id)
 	res.render('course', {
